29f3fbf1e81ddbb293d3f8aa71244c7585111797
SKARDXII <124432259+SKARDXII@users.noreply.github.com>
2024-12-19 10:04:52 +0300

    Update task2.c

--- 3824Б1ПР4/13 Леванов Илья Александрович/task2.c
+++ 3824Б1ПР4/13 Леванов Илья Александрович/task2.c
@@ -1,8 +1,38 @@
-// Задача 2. Дан непустой массив A, содержащий n натуральных чисел.
-// Назовем «серией» группу подряд идущих одинаковых элементов, 
-// а длиной серии – количество этих элементов (длина серии может быть равна 1).
-// Найти длину наибольшей серии.
-int task2(unsigned A[], size_t n)
-{
-  return -1;
+// // Задача 2. Дан непустой массив A, содержащий n натуральных чисел.
+// // Назовем «серией» группу подряд идущих одинаковых элементов, 
+// // а длиной серии – количество этих элементов (длина серии может быть равна 1).
+// // Найти длину наибольшей серии.
+// int task2(unsigned A[], size_t n)
+// {
+//   return -1;
+// }
+
+#include "stdio.h"
+#include "stddef.h"
+
+int task2(unsigned A[], size_t n) {
+    if (n == 0) {
+        return 0;  
+    }
+
+    int max_length = 1;  
+    int current_length = 1;  
+
+    for (size_t i = 1; i < n; i++) {
+        if (A[i] == A[i - 1]) {
+            current_length++;  
+        }
+        else {
+            if (current_length > max_length) {
+                max_length = current_length;  
+            }
+            current_length = 1;  
+        }
+    }
+
+    if (current_length > max_length) {
+        max_length = current_length;
+    }
+
+    return max_length;
 }
